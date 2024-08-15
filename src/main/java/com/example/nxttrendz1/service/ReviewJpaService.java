a9c771da0a4985430bd913d1aed009308caf2c0a
Ravalidyaga <ravali.dyaga25@gmail.com>
2024-08-15 18:42:55 +0000

    solution

--- src/main/java/com/example/nxttrendz1/service/ReviewJpaService.java
+++ src/main/java/com/example/nxttrendz1/service/ReviewJpaService.java
@@ -1,17 +1,6 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.http.HttpStatus;
- * import org.springframework.stereotype.Service;
- * import org.springframework.web.server.ResponseStatusException;
- * import java.util.ArrayList;
- * import java.util.List;
- * 
- */
 
-// Write your code here
+
+
 package com.example.nxttrendz1.service;
 
 import com.example.nxttrendz1.model.Review;
@@ -19,7 +8,7 @@ import com.example.nxttrendz1.model.Product;
 import com.example.nxttrendz1.repository.ProductJpaRepository;
 import com.example.nxttrendz1.repository.ReviewJpaRepository;
 import com.example.nxttrendz1.repository.ReviewRepository;
-
+user
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
